35a14f48fa66d553f765af2bc9b91f506b3a35e3
Johan Steenhoven <johan.steenhoven@nn-group.com>
2018-11-04 16:10:21 +0100

    use functions

--- lambda_handler.py
+++ lambda_handler.py
@@ -2,10 +2,7 @@ import xmltodict
 import requests
 
 
-def loadRSS():
-    # justadded
-    url = 'https://trailers.apple.com/trailers/home/xml/newest_720p.xml'
-
+def loadRSS(url):
     # creating HTTP response object from given url
     resp = requests.get(url)
 
@@ -16,19 +13,25 @@ def loadRSS():
     return xmltodict.parse(resp.content)
 
 
-rss = loadRSS()
+def createTrailerPage(rss):
 
-heading = '<html><head><title>this is the title</title></head><body>'
-print(heading)
+    rss = loadRSS(rss)
 
-# print(doc['records']['movieinfo']['info']['title']['#text'])
-for movie in rss['records']['movieinfo']:
-    title = movie['info']['title']
-    url = movie['preview']['large']['#text']
-    thumb = movie['poster']['location']
-    # print(title + " " + url + " " + poster)
-    href = ('<a href="' + url + '"><img src="' + thumb + '" title="' + title + '"></a>')
-    print(href)
-    # <a href=" "><img src="flower.jpg" width="82" height="86" title="White flower" alt="Flower"></a>
+    with open('newest_720p.html', 'wt') as f:
+        # f.flush()
+        f.write('<html><head><title>this is the title</title></head><body>')
 
-print('</body></html>')
+    for movie in rss['records']['movieinfo']:
+        title = movie['info']['title']
+        url = movie['preview']['large']['#text']
+        thumb = movie['poster']['location']
+
+        with open('newest_720p.html', 'a') as f:
+
+            f.write('<a href="' + url + '"><img src="' + thumb + '" title="' + title + '"></a>')
+    with open('newest_720p.html', 'a') as f:
+        f.write('</body></html>')
+        f.close()
+
+
+createTrailerPage('https://trailers.apple.com/trailers/home/xml/newest_720p.xml')
